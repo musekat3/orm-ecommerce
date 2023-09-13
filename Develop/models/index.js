d1b74bb05b3129c9f5df2d9950beb1e9ab1a5fd0
Kathy Hyde <musekat3@gmail.com>
2023-09-13 10:36:54 -0700

    Updates

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -12,6 +12,65 @@ const ProductTag = require('./ProductTag');
 
 // Tags belongToMany Products (through ProductTag)
 
+class Product extends Model {}
+
+Product.init({
+}, {
+  sequelize,
+  modelName: 'product',
+});
+
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
+
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
+
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Category extends Model {}
+
+Category.init({
+}, {
+  sequelize,
+  modelName: 'category',
+});
+
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
+
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Tag extends Model {}
+
+Tag.init({
+}, {
+  sequelize,
+  modelName: 'tag',
+});
+
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
+
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class ProductTag extends Model {}
+
+ProductTag.init({
+}, {
+  sequelize,
+  modelName: 'product_tag',
+});
+
 module.exports = {
   Product,
   Category,
